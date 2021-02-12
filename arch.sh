8ec0d8a84003e52839ede2f74764417181114d4a
Martin Sione <sionemartin@hotmail.com>
2021-02-12 19:03:19 -0300

    added variables

--- arch.sh
+++ arch.sh
@@ -4,15 +4,36 @@ pacman -Sy --noconfirm dialog || { echo "Error at script start: Are you sure you
 
 dialog --defaultno --title "Welcome to Martin's Arch automated installation" --yesno "This is intended for personal use.\n\nTo stop this script, press no."  10 60 || exit
 
-dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> hostname
+dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> hostname.tmp
+
+dialog --no-cancel --inputbox "Enter a password for your computer." 10 60 2> password.tmp
 
 dialog --no-cancel --inputbox "Enter the disk in which you want to install the system." 10 60 2> disk.tmp
 
-disk=$(cat mnt/etc/disk.tmp)
-
 timedatectl set-ntp true
 
-cat <<EOF | fdisk /dev/$disk
+# cat <<EOF | fdisk /dev/sda
+# Set partition table to gpt
+# Creates a new partition
+# Enter
+# Enter
+# Select the size+550M
+# Creates a new partition
+# Enter
+# Enter
+# Enter => Uses all the space left
+# Change the tipe of the partition
+# Of partition 1
+# To tipe 1 (EFI System)
+# Write the changes
+# EOF
+
+# Insert the name without the /
+export hostname=$(cat hostname.tmp)
+export password=$(cat password.tmp)
+export disk=$(cat disk.tmp)
+
+cat <<EOF | fdisk /dev/${disk}
 g
 n
 
@@ -27,47 +48,48 @@ t
 1
 w
 EOF
-partprobe
 
 mkfs.fat -F32 /dev/${disk}1 # Make first partition (+550M) f32
 mkfs.ext4 /dev/${disk}2     # Make the second partition(left space on the drive) ext4
+
 mount /dev/${disk}2 /mnt    # Mount the big partition
 
-pacstrap /mnt base base-devel linux-lts linux-firmware
+# Install base and kernel
+pacstrap /mnt base linux-lts linux-firmware --needed base-devel
 
 genfstab -U /mnt >> /mnt/etc/fstab    # Generate filesystem table
 arch-chroot /mnt                      # Change to root of the installation
 
+# Select time zone
 ln -sf /usr/share/zoneinfo/America/Buenos_Aires /etc/localtime
+# Set hardware clock
 hwclock --systohc
 
-ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
-hwclock --systohc
-
+# Set the locale
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
-# Hostname and passwd
-mv hostname /mnt/etc
-hostname=$(cat mnt/etc/hostname)
+# Grub config for uefi
+mkdir /boot/EFI
+mount /dev/${disk}1 /boot/EFI
+pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Hostname and password
+echo ${hostname} >> /etc/hostname
+
 cat >> /etc/hosts <<EOF
 127.0.0.1       localhost
 ::1             localhost
 127.0.1.1       ${hostname}.localdomain     ${hostname}
 EOF
 
-passwd
+passwd # probar ponerlo a lo ult?
+$password
+$password
 
-# Grub config for uefi
-pacman -S --noconfirm grub efibootmgr dosfstoolgs os-prober mtools
-mkdir /boot/EFI
-mount /dev/${disk}1 /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# NetworkManager
-pacman --noconfirm --needed -S networkmanager
+pacman -S --noconfirm networkmanager git
 systemctl enable NetworkManager
-systemctl start NetworkManager
 
-clear
+umount -l /mnt
