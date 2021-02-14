1c0f05845b853b9936f1f2de206ee775b6153791
Martin Sione <sionemartin@hotmail.com>
2021-02-13 23:55:07 -0300

    add dialog installation on arch-chroot

--- install/arch.sh
+++ install/arch.sh
@@ -84,6 +84,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
+pacman -Sy --noconfirm dialog || { echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?"; exit; }
 dialog --defaultno --title "Welcome to Martin's Arch automated installation" --yesno "Do you want to create your user and set up your password?."  10 60 || exit
 
 export install_path=https://raw.githubusercontent.com/martinsione/dotfiles/testing/install
