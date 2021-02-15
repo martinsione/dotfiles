1f35bdc8ba98fe427a9510dfb2580a12c8fd7c22
Martin Sione <sionemartin@hotmail.com>
2021-02-15 16:35:52 -0300

    dwm config.h update

--- src/.local/src/dwm/config.h
+++ src/.local/src/dwm/config.h
@@ -4,10 +4,10 @@
 /* Appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -41,8 +41,8 @@ static const unsigned int alphas[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-/* static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " }; */
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -54,10 +54,10 @@ static const Rule rules[] = {
 	{ "Brave-browser",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "zoom",           NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "mpv",            NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Thunderbird",    NULL,       NULL,       1 << 4,       0,           -1 },
-	{ "Gimp",           NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Thunderbird",    NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Gimp",           NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Steam",          NULL,       NULL,       1 << 4,       0,           -1 },
 	{ "Virt-manager",   NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "Steam",          NULL,       NULL,       1 << 6,       0,           -1 },
 	{ "discord",        NULL,       NULL,       1 << 7,       0,           -1 },
 	{ "Pcmanfm",        NULL,       NULL,       1 << 8,       0,           -1 },
 };
@@ -175,7 +175,7 @@ static Key keys[] = {
 	{ MODKEY,					              XK_f,      togglefullscr,  {0} },
 
 	// View tags
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	/* { MODKEY,                       XK_0,      view,           {.ui = ~0 } }, */
 	/* { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, */
 
 	// Multiple monitors
@@ -199,22 +199,22 @@ static Key keys[] = {
 
 	//-------------------------- Gaps ----------------------------------
 
-	{ Mod4Mask,						          XK_0,      togglegaps,     {0} },
-	/* { MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } }, */
-	/* { MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} }, */
+	{ Mod4Mask,               XK_0,      togglegaps,     {0} },
+	{ Mod4Mask,               XK_u,      incrgaps,       {.i = +1 } },
+	{ Mod4Mask|ShiftMask,     XK_u,      incrgaps,       {.i = -1 } },
+	{ Mod4Mask,               XK_i,      incrigaps,      {.i = +1 } },
+	{ Mod4Mask|ShiftMask,     XK_i,      incrigaps,      {.i = -1 } },
+	{ Mod4Mask,               XK_o,      incrogaps,      {.i = +1 } },
+	{ Mod4Mask|ShiftMask,     XK_o,      incrogaps,      {.i = -1 } },
+	{ Mod4Mask,               XK_6,      incrihgaps,     {.i = +1 } },
+	{ Mod4Mask|ShiftMask,     XK_6,      incrihgaps,     {.i = -1 } },
+	{ Mod4Mask,               XK_7,      incrivgaps,     {.i = +1 } },
+	{ Mod4Mask|ShiftMask,     XK_7,      incrivgaps,     {.i = -1 } },
+	{ Mod4Mask,               XK_8,      incrohgaps,     {.i = +1 } },
+	{ Mod4Mask|ShiftMask,     XK_8,      incrohgaps,     {.i = -1 } },
+	{ Mod4Mask,               XK_9,      incrovgaps,     {.i = +1 } },
+	{ Mod4Mask|ShiftMask,     XK_9,      incrovgaps,     {.i = -1 } },
+	{ Mod4Mask|ShiftMask,     XK_0,      defaultgaps,    {0} },
 
 
 	//-------------------------- Apps ----------------------------------
@@ -224,8 +224,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p, 	   spawn,		   {.v = dmenucmd } },
 	{ MODKEY,                       XK_d,      spawn,	 	   SHCMD("discord") },
 	{ MODKEY,                       XK_w,      spawn,	 	   SHCMD("$BROWSER") },
-	{ MODKEY,                       XK_v,      spawn,	 	   SHCMD("$GUIEDITOR") },
-	{ MODKEY,                       XK_e,      spawn,	 	   SHCMD("$GUIFILE") },
+	{ MODKEY,                       XK_v,      spawn,	 	   SHCMD("$GEDITOR") },
+	{ MODKEY,                       XK_e,      spawn,	 	   SHCMD("$GFILE") },
 	{ MODKEY,                       XK_m,      spawn,	 	   SHCMD("$MAIL") },
 	{ MODKEY,                       XK_r,      spawn,	 	   SHCMD(TERMINAL " -e $FILE") },
 
