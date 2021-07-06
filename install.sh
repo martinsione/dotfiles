b395147d297824bb390b9f569eded608f098609b
Martin Sione <martinsione03@gmail.com>
2021-07-06 17:55:37 -0300

    Fix if statements

--- install.sh
+++ install.sh
@@ -14,24 +14,24 @@ symlinks=(
   .config/nvim
   .config/tmux
   .config/zsh
+  .zprofile
 )
 
 
 clone_repo() {
-  if [ ! -f "$HOME/dotfiles" ]; then 
+  if [ -d "$HOME/dotfiles" ]; then
     mv "$HOME/dotfiles" "${dotfiles_dir}";
-  elif [ ! -f "${dotfiles_dir}" ]; then
+  elif [ ! -d "${dotfiles_dir}" ]
     git clone "${repo_host}${repo_path}" "${dotfiles_dir}"
   fi
 
-  if [ -f "${dotfiles_dir}" ]; then
-    cd ${dotfiles_dir}
-    git submodule update --init --recursive
+  if [ -d "${dotfiles_dir}" ]; then 
+    cd ${dotfiles_dir} && git submodule update --init --recursive
   fi
 }
 
-# arr=(); arr=("${arr[@]}" "sudo ln -s $HOME/blabla /usr/bin/blabla")
 symlink_files() {
+  # arr=(); arr=("${arr[@]}" "sudo ln -s $HOME/blabla /usr/bin/blabla")
   # readarray -t links < $(awk -F ":" '!/^ *#/ && NF {print "ln -sr $HOME/.dotfiles/" $2 " $HOME/" $NF}' "${symlinks_file}")
   # readarray -t links < "${symlinks_file}"
 
@@ -41,7 +41,7 @@ symlink_files() {
 
   for name in "${symlinks[@]}"; do
     if [ ! -e "$name" ]; then
-      ln -sfv "${dotfiles_dir}/src/${name}" "$HOME/${name}"
+      ln -srfv "${dotfiles_dir}/src/${name}" "$HOME/${name}"
     else
       echo "${name} already exists."
     fi
@@ -53,7 +53,7 @@ symlink_files() {
 # }
 
 change_shell() {
-  sudo chsh -s /bin/zsh "${USER}" >/dev/null 2>&1
+  sudo chsh -s /usr/bin/zsh "${USER}" >/dev/null 2>&1
 }
 
 
