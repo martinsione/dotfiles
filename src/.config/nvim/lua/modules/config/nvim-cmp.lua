b4237377b817aa24f9c7cac824432fe6a3916c30
Martin Sione <martinsione03@gmail.com>
2021-09-05 01:15:22 -0300

    Add snippet jumping with <Tab> and <S-Tab>

--- src/.config/nvim/lua/modules/config/nvim-cmp.lua
+++ src/.config/nvim/lua/modules/config/nvim-cmp.lua
@@ -1,5 +1,15 @@
 return function()
   local cmp = require 'cmp'
+
+  local check_back_space = function()
+    local col = vim.fn.col '.' - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
+  end
+
+  local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+  end
+
   cmp.setup {
     preselect = 'item',
     snippet = {
@@ -11,8 +21,32 @@ return function()
       ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if vim.fn.pumvisible() == 1 then
+          vim.fn.feedkeys(t '<C-n>', 'n')
+        elseif vim.fn.call('vsnip#available', { 1 }) == 1 then
+          vim.fn.feedkeys(t '<Plug>(vsnip-expand-or-jump)', '')
+        elseif check_back_space() then
+          vim.fn.feedkeys(t '<Tab>', 'n')
+        else
+          fallback()
+        end
+      end, {
+        'i',
+        's',
+      }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if vim.fn.pumvisible() == 1 then
+          vim.fn.feedkeys(t '<C-p>', 'n')
+        elseif vim.fn.call('vsnip#jumpable', { -1 }) == 1 then
+          vim.fn.feedkeys(t '<Plug>(vsnip-jump-prev)', '')
+        else
+          fallback()
+        end
+      end, {
+        'i',
+        's',
+      }),
     },
     sources = {
       { name = 'buffer' },
