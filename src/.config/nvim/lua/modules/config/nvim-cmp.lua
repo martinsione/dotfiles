66cf0f27851a85c51b1ea73854ad62e122ea6ab1
Martin Sione <martinsione03@gmail.com>
2022-02-10 23:22:04 -0300

    some updates on cmp config

--- src/.config/nvim/lua/modules/config/nvim-cmp.lua
+++ src/.config/nvim/lua/modules/config/nvim-cmp.lua
@@ -16,19 +16,22 @@ return function()
     },
     formatting = {
       format = lspkind.cmp_format {
-        with_text = false,
-        menu = {
-          buffer = '[buf]',
-          nvim_lsp = '[LSP]',
-          nvim_lua = '[api]',
-          path = '[path]',
-          luasnip = '[snip]',
-        },
+        mode = 'symbol_text', -- show only symbol annotations
+        preset = 'codicons', -- https://github.com/microsoft/vscode-codicons/raw/main/dist/codicon.ttf
+        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       },
     },
     experimental = {
       native_menu = false,
       ghost_text = true,
+    },
+    -- Sources order are actually their priority order
+    sources = cmp.config.sources {
+      { name = 'nvim_lua' },
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+      { name = 'path' },
+      { name = 'emoji' },
     },
     mapping = {
       ['<C-e>'] = cmp.mapping.close(),
@@ -42,10 +45,7 @@ return function()
         else
           fallback()
         end
-      end, {
-        'i',
-        's',
-      }),
+      end, { 'i', 's' }),
       ['<S-Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
@@ -54,16 +54,7 @@ return function()
         else
           fallback()
         end
-      end),
-    },
-    -- Sources order are actually their priority order
-    sources = {
-      { name = 'nvim_lua' },
-      { name = 'nvim_lsp' },
-      { name = 'luasnip', keyword_length = 2 },
-      { name = 'buffer', keyword_length = 5 },
-      { name = 'path' },
-      { name = 'emoji' },
+      end, { 'i', 's' }),
     },
   }
 end
